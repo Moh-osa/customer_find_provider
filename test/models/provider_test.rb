1b1b750a5ef585695ba187a1f4dc7b5acc30429a
M-Osa <mohamed.abdeldayem@doctolib.com>
2021-07-11 23:04:37 +0200

    basic model test

--- test/models/provider_test.rb
+++ test/models/provider_test.rb
@@ -3,7 +3,41 @@
 require 'test_helper'
 
 class ProviderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    # 1.70 km away from 52.5087612, 13.3733575
+    Provider.create(
+      name: 'wood provider',
+      lat: 52.512963,
+      lng: 13.398775,
+      operating_radius: 1650,
+      average_rating: 4,
+      provider_specialities: [ProviderSpeciality.create(category: 'flooring', material: 'wood')]
+    )
+    # 3.24 km away from 52.5087612, 13.3733575
+    Provider.create(
+      name: 'carpet provider',
+      lat: 52.534632,
+      lng: 13.396429,
+      operating_radius: 3300,
+      average_rating: 4,
+      provider_specialities: [ProviderSpeciality.create(category: 'flooring', material: 'carpet')]
+    )
+  end
+
+  test 'supports_category_and_material selects correct material' do
+    results = Provider.supports_category_and_material('flooring', 'wood')
+    assert_equal(1, results.size)
+    assert_equal('wood provider', results[0].name)
+  end
+
+  test 'supports_category_and_material selects correct category' do
+    results = Provider.supports_category_and_material('other', 'wood')
+    assert_equal(0, results.size)
+  end
+
+  test 'within_operating_radius selects matching providers withing provider radius' do
+    results = Provider.within_operating_radius(52.5087612, 13.3733575)
+    assert_equal(1, results.size)
+    assert_equal('carpet provider', results[0].name)
+  end
 end
