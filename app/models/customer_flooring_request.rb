e7e80e89f0f5cf75cb8c1819c0acb979d71523d9
M-Osa <mohamed.abdeldayem@doctolib.com>
2021-07-11 19:41:08 +0200

    more validations

--- app/models/customer_flooring_request.rb
+++ app/models/customer_flooring_request.rb
@@ -5,6 +5,9 @@ class CustomerFlooringRequest < ApplicationRecord
 
   # validates :material, inclusion: { in: materials.values }
   validates :lat, :lng, :phone_number, :square_meters, :material, presence: true
+  validates :lat, numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to: 90 }
+  validates :lng, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }
+  validates :square_meters, numericality: { greater_than: 0 }
 
   def self.valid_material?(query)
     materials.values.include? query
